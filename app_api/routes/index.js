4b5b0525e0c7b0dcb5a97e6c354083ded34fa1a2
sdbrasher <sdbrasher@gmail.com>
2022-11-26 08:02:57 -0600

    Module 5, implementing RESTApi

---
+++ app_api/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const tripsController = require('../controllers/trips');
+
+
+router
+    .route('/trips')
+    .get(tripsController.tripsList);
+
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindCode);
+
+module.exports = router;
